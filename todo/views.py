d75a3ffc39c555b08520ef83324d65860646d5b2
nschcur <nazae530@gmail.com>
2024-01-23 21:46:06 +0200

    created views.py

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from .forms import TaskForm
+from .models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = ("todo/task_list.html")
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+    form_class = TaskForm
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+    form_class = TaskForm
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TaskStatusUpdate(Task, generic.View):
+    @staticmethod
+    def post(request: HttpRequest, task_id: int) -> HttpResponseRedirect:
+        tasks = Task.objects.get(pk=task_id)
+        TaskStatusUpdate.toggle_status(tasks)
+        return redirect("todo:task-list")
+
+    def toggle_status(self):
+        self.is_done = not self.is_done
+        self.save()
