373ffce8c689028afcac0c2e7bd6a555225990da
nschcur <nazae530@gmail.com>
2024-01-23 21:40:19 +0200

    created models.py Task and Tag

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField(null=False, blank=False)
+    created = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
